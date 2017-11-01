8eacbd260fdc569eefe51aac34a0fafdd9c334b8
Andrew Lipsey <adlipsey@gmail.com>
2017-11-01 15:00:06 -0400

    Added db login and account creation to index.html

--- assets/javascript/latchkeyper.js
+++ assets/javascript/latchkeyper.js
@@ -1,5 +1,5 @@
   // Initialize Firebase
-  var config = {
+  /*var config = {
     apiKey: "AIzaSyDYUmxcgXcMpWsHt_GGtQSsw2nukEurl_c",
     authDomain: "latchkeyper.firebaseapp.com",
     databaseURL: "https://latchkeyper.firebaseio.com",
@@ -7,7 +7,7 @@
     storageBucket: "latchkeyper.appspot.com",
     messagingSenderId: "1055724083998"
   };
-  firebase.initializeApp(config);
+  firebase.initializeApp(config);*/
   //End required firebase code
 
 
@@ -42,38 +42,38 @@ function checkPoints () {
 
 $(document).ready(function() {
 
-$("button").click(message);
-$(document).on("click", "#message-parents-button", message);
-message ();
-});
-function message () {
+  $("button").click(message);
+  $(document).on("click", "#message-parents-button", function(){
+  message ();
+  });
+
+  function message () {
   var msg = $("#new-msg").val();
   $("#messages").append("<div>" + msg + "<span id='delete'>X</span></div>");
   $("#new-msg").val("");
+  } 
 
-}
 
+  $("button").click(addTask);
+  $(document).on("click", "#delete", removeTask);
 
-$("button").click(addTask);
-$(document).on("click", "#delete", removeTask);
-
-$("input").keypress(function(event) {
+  $("input").keypress(function(event) {
   //13 is the ascii code for the enter key
   if (event.which === 13) {
     addTask();
   }
-});
-// Function to add a task.
-function addTask() {
+  });
+  // Function to add a task.
+  function addTask() {
   var task = $("#new-task").val();
   $("#tasks").append("<div>" + task + "<span id='delete'>X</span></div>");
   $("#new-task").val("");
-}
-// Function to remove a task.
-function removeTask() {
-  // Grab the closest div to the element that was clicked and remove it.
-  // (In this case, the closest element is the one that's encapsulating it.)
-  $(this).closest("div").remove();
-}
+  }
+  // Function to remove a task.
+  function removeTask() {
+    // Grab the closest div to the element that was clicked and remove it.
+    // (In this case, the closest element is the one that's encapsulating it.)
+    $(this).closest("div").remove();
+  }
 
 });
